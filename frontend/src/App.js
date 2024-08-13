8f007e144a67390dd35c83b4c5f5deb40c41eb94
Shaswat-31 <143761992+Shaswat-31@users.noreply.github.com>
2024-08-13 20:39:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Flashcard from "./components/flashcard";
 import Dashboard from "./components/dashboard";
-import { Button } from "@chakra-ui/react";
+import { Button, Spinner } from "@chakra-ui/react";
 import "./App.css";
 
 const App = () => {
   const [flashcards, setFlashcards] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [toggle, setToggle] = useState(false);
+  const [loading, setLoading] = useState(true); // New state for loading
 
   useEffect(() => {
     fetchFlashcards();
   }, []);
 
   const fetchFlashcards = async () => {
-    const response = await axios.get("https://flashcard-app-tuf.onrender.com/api/flashcards");
-    setFlashcards(response.data);
+    try {
+      const response = await axios.get(
+        "https://flashcard-app-tuf.onrender.com/api/flashcards"
+      );
+      setFlashcards(response.data);
+    } catch (error) {
+      console.error("Error fetching flashcards:", error);
+    } finally {
+      setLoading(false); // Set loading to false after data is fetched
+    }
   };
 
   const handleNext = () => {
@@ -36,10 +45,20 @@ const App = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 h-screen gap-4 p-4">
       <div className="flex flex-col items-center justify-center">
-        {flashcards.length > 0 ? (
+        {loading ? (
+          <Spinner
+            size="xl"
+            color="teal.500"
+            thickness="4px"
+            emptyColor="gray.200"
+            speed="0.65s"
+            label="Loading"
+          />
+        ) : flashcards.length > 0 ? (
           <Flashcard flashcard={flashcards[currentIndex]} resetFlip={toggle} />
-        ):
-        <Flashcard flashcard={"Nothing to Show"} resetFlip={toggle}/>}
+        ) : (
+          <Flashcard flashcard={"Nothing to Show"} resetFlip={toggle} />
+        )}
         <div className="mt-4 flex space-x-4">
           <Button colorScheme="teal" variant="outline" onClick={handlePrevious}>
             Previous
